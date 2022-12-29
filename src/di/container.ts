b95dee271c1d78c722d2ddb0e3eb4d4ac8322daf
Davi Nascimento <davign20@outlook.com>
2022-12-29 20:40:18 -0300

    Adding cache and Docker compose

--- src/di/container.ts
+++ src/di/container.ts
@@ -1,8 +1,12 @@
 import { container } from 'tsyringe';
 import { PrismaClient } from '@prisma/client';
+import { ICacheService, RedisCacheService } from '@services/cache';
 
 container.register<PrismaClient>('PrismaClient', {
   useValue: new PrismaClient(),
+});
+container.register<ICacheService>('CacheService', {
+  useValue: new RedisCacheService(),
 });
 
 import './city';
--- src/useCases/user/get.ts
+++ src/useCases/user/get.ts
@@ -1,5 +1,5 @@
 import { inject, injectable } from 'tsyringe';
-
+import { User, City } from '@prisma/client';
 import { toUserCityResponse } from '@maps/user';
 import { IUserRepository } from '@repositories/user';
 import {
@@ -7,12 +7,28 @@ import {
   NotFoundPresenter,
   SuccessPresenter,
 } from '@presenters/index';
+import { ICacheService } from '@services/cache';
 
 @injectable()
 export class GetUserUseCase {
-  constructor(@inject('UserRepository') private repository: IUserRepository) {}
+  constructor(
+    @inject('UserRepository') private repository: IUserRepository,
+    @inject('CacheService') private cacheService: ICacheService
+  ) {}
 
   async handle(id: string): Promise<IPresenter> {
+    const cachedUserKey = `user-${id}`;
+    const cachedUser = await this.cacheService.get<User & { city: City }>(
+      cachedUserKey
+    );
+
+    if (cachedUser) {
+      console.log(`User ${id} is cached, returning it`);
+      const result = toUserCityResponse(cachedUser);
+      return new SuccessPresenter(result);
+    }
+
+    console.log(`Getting user ${id} from database`);
     const user = await this.repository.findById(id);
 
     if (!user) {
@@ -20,6 +36,7 @@ export class GetUserUseCase {
     }
 
     const result = toUserCityResponse(user);
+    await this.cacheService.set(cachedUserKey, result);
     return new SuccessPresenter(result);
   }
 }
