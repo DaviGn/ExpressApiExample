0645d8e8961bbb3df9b45872dadb51064cea4033
Davi Guaracho Nascimento <davi.nascimento@avanade.com>
2023-03-09 12:48:16 -0300

    Fixing pipeline

--- tests/context.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createMockContext = void 0;
-const jest_mock_extended_1 = require("jest-mock-extended");
-const createMockContext = () => ({
-    prisma: (0, jest_mock_extended_1.mockDeep)()
-});
-exports.createMockContext = createMockContext;
